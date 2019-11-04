ebe33d93fa67ec0778d663450ac32abf61c8b756
Nicolás <nicolas.villegas@solenium.co>
2019-11-03 23:54:06 -0500

    Avances. sensorActividadCardiaca() detecta pulsos del corazon (entradas de cualquier caracter con el teclado) y calcula la frecuencia promedio de las pulsaciones. Si la frecuencia estaa+a por debajo de lo normal, genera una alerta

--- Proyecto_Marcapasos/marcapasos.c
+++ Proyecto_Marcapasos/marcapasos.c
@@ -1,21 +1,68 @@
 #include <stdio.h>
-#include <time.h>
+#include <sys/time.h>
 
 #include "marcapasos.h"
 
+#define TOTAL_MUESTRAS 3
+#define FREC_NORMAL 60
+
+
 void sensorActividadCardiaca(void) {
 
-    static clock_t start_t;
+    double start_t, delta_t, BPM;
+    double frecBPM[TOTAL_MUESTRAS];
+    int indice = 0;
 
-    printf("Comenzando temporizador de 10 segundos...\n");
+    while (1) {
 
-    for (int i = 10; i > 0; --i) {
-        start_t = clock();
-        while ((clock() - start_t) / CLOCKS_PER_SEC < 1.0) {}
-        printf("%d\n", i);
+        start_t = tiempoSegundos();
+        getchar();
+        delta_t = (tiempoSegundos() - start_t);
+        BPM = 60 / delta_t; // Latidos por minuto
+        frecBPM[indice++ % TOTAL_MUESTRAS] = BPM;
+
+        if (indice > 5) {
+            double f_prom = analizarFrecuenciaCardiaca(frecBPM, TOTAL_MUESTRAS);
+            printf("%d. Frecuencia cardiaca promedio: %.1f BPM\n", indice, f_prom);
+        }
+
     }
 
-    printf("Listo!!\n");
-
     return;
+
+}
+
+double analizarFrecuenciaCardiaca(double *arr, int n) {
+
+    double frecPromedio = promedioArreglo(arr, n);
+
+    if (frecPromedio < FREC_NORMAL) {
+        printf("¡Frecuencia cardiaca por debajo de lo normal!");
+    }
+
+    return frecPromedio;
+    
+}
+
+
+/*
+
+    <--- Funciones auxiliares --->
+
+*/
+
+double tiempoSegundos(void) {
+
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return (double) tv.tv_sec + tv.tv_usec/1e6;
+
+}
+
+double promedioArreglo(double *a, int n) {
+    double suma = 0;
+    for (int i = 0; i < n; ++i) {
+        suma += a[i];
+    }
+    return suma / (double) n;
 }
--- Proyecto_Marcapasos/marcapasos.h
+++ Proyecto_Marcapasos/marcapasos.h
@@ -1,7 +1,12 @@
 #ifndef MARCAPASOS_H
 #define MARCAPASOS_H
 
+// Funciones auxiliares:
+double promedioArreglo(double*, int);
+double tiempoSegundos(void);
+
+// Funciones del problema:
+double analizarFrecuenciaCardiaca(double*, int);
 void sensorActividadCardiaca(void);
-//float calcularFrecuenciaCardiaca(int*);
 
 #endif
